75b5744565d198b9410e69c5624b94a592756744
CeraChen <1059315177@qq.com>
2023-12-05 14:40:58 +0800

    error detail position self-adaptation

--- src/page/report.js
+++ src/page/report.js
@@ -5,16 +5,20 @@ import response_json from '../text/response_sample2.json'
 
 let boundary = 70;
 
-function ScoreBar({ score }) {
+function ScoreBar({ score , overall }) {
     const progressPercentage = (score / 9) * 100;
     console.log(score);
+
+    var mHeight = (overall)? "18px" : "17px";
+    var mColor = (overall)? "mediumseagreen" : "lightgreen";
+    var mBgColor = (overall)? "slategray" : "lightslategray";
   
     return (
-      <div className="score_bar">
+      <div>
         <div className="progress_bar" style={{ width: `${progressPercentage}%`}}>
-          <button className="short_bar"></button>
+          <button className="short_bar" style={{ height: `${mHeight}`, backgroundColor: `${mColor}` }}></button>
         </div>
-        <button className="long_bar"></button>
+        <button className="long_bar" style={{ height: `${mHeight}`, backgroundColor: `${mBgColor}`}}></button>
       </div>
     );
   }
@@ -47,6 +51,44 @@ function Report() {
     };
 
 
+    const getErrorDetailPosition = () => {
+        if (spanRef.current) {
+            const errorDetail = spanRef.current.querySelector(".error_detail");
+            const errorDetailWidth = errorDetail.offsetWidth;
+            console.log("errorDetail.offsetWidth");
+            console.log(errorDetail.offsetWidth);
+            const screenWidth = window.innerWidth;
+
+            const word = document.getElementById(expandedItemIndex.toString());
+            const wordLeft = word.offsetLeft;
+
+            console.log("spanRef.current.getBoundingClientRect().left");
+            console.log(spanRef.current.getBoundingClientRect().left);
+            console.log("wordLeft");
+            console.log(wordLeft);
+
+            if(wordLeft < 0){
+                errorDetail.style.left = `${wordLeft}px`;
+                errorDetail.style.top = "56px";
+            }
+            else{
+                if (errorDetailWidth + spanRef.current.getBoundingClientRect().left > screenWidth) {
+                    errorDetail.style.left = "auto";
+                    errorDetail.style.right = "0";
+                } else {
+                    errorDetail.style.left = "0";
+                    errorDetail.style.right = "auto";
+                }
+            }
+        
+        }
+    };
+
+    useEffect(() => {
+        getErrorDetailPosition();
+    }, [expandedItemIndex]);
+
+
     useEffect(() => {
         document.addEventListener("mousedown", handleOutsideClick);
         return () => {
@@ -56,102 +98,123 @@ function Report() {
 
 
     return (
-        <div className="text_area">
-            {response_json["speech_score"]["word_score_list"].map((item, index) => {
-                var syllable_marks = new Array(item.phone_score_list.length).fill(0);
-                var syllables = new Array(item.phone_score_list.length).fill('');
-                var last_pos = 0;
-                for(var syllable of item.syllable_score_list) {
-                    syllable_marks[last_pos] = syllable.phone_count;
-                    syllables[last_pos] = syllable.letters;
-                    last_pos += syllable.phone_count;
-                }
-                               
-
-
-                return (
-                    <span className="container" ref={(expandedItemIndex === index)? spanRef : null}>
-                        {expandedItemIndex === index && (
-                            <span className="error_detail">
-                                <table className="phone_tab">
-                                    <thead>
-                                        <tr>
-                                            <th>Syllable</th>
-                                            <th>Phone</th>
-                                            <th>Result</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {item.phone_score_list.map((phone, idx) => (
-                                            <tr>
-                                                {(syllable_marks[idx] > 0) && (<td rowSpan={syllable_marks[idx]}>{syllables[idx]}</td>)}
-                                                <td className={(phone.sound_most_like === phone.phone)? "correct" : "incorrect"}>{phone.phone}</td>
-                                                <td className={(phone.sound_most_like === phone.phone)? "correct" : "incorrect"}>{(phone.sound_most_like)? ((phone.sound_most_like === phone.phone)? 'Good' : phone.sound_most_like) : '[missing]'}</td>
-                                            </tr>
-                                        )
-                                        )}
-                                    </tbody>
-                                </table>
-                            </span>
-                        )}
-                        <span id={index.toString()} 
-                            className={(item.quality_score > boundary)? 
-                                ((expandedItemIndex === index)? "active_correct" : "correct") : 
-                                ((expandedItemIndex === index)? "active_incorrect" : "incorrect")} 
-                            onClick={() => handleSpanClick(index)}>
-                            {item.word}
-                        </span>
-                        <span className="correct">{item.ending_punctuation} </span>
-                    </span>);
-                }
-            )}
+        <div className="report_main">
+            <p id="part">Part A</p>
             <div className="score_container">
-                <div className="scores">
-                    {/* <div className="overall_score">IELTS overall: {response_json["speech_score"]["ielts_score"]["overall"]}</div> */}
-                    <div className="ielts_scores">
-                        <table className="ielts_score_table">
-                            <thead>
-                                <tr>
-                                    <th className="overall_score_text">IELTS overall:</th>
-                                    <th className="overall_score_value">{response_json["speech_score"]["ielts_score"]["overall"]}</th>
-                                    <th><ScoreBar score={response_json["speech_score"]["ielts_score"]["overall"]}></ScoreBar></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td className="sub_score_text">Pronunciation:</td>
-                                    <td className="sub_score_value">{response_json["speech_score"]["ielts_score"]["pronunciation"]}</td>
-                                    <td><ScoreBar score={response_json["speech_score"]["ielts_score"]["pronunciation"]}></ScoreBar></td>
-                                </tr>
-                                <tr>
-                                    <td className="sub_score_text">Fluency:</td>
-                                    <td className="sub_score_value">{response_json["speech_score"]["ielts_score"]["fluency"]}</td>
-                                    <td><ScoreBar score={response_json["speech_score"]["ielts_score"]["fluency"]}></ScoreBar></td>
-                                </tr>
-                                <tr>
-                                    <td className="sub_score_text">Coherence:</td>
-                                    <td className="sub_score_value">{response_json["speech_score"]["ielts_score"]["coherence"]}</td>
-                                    <td><ScoreBar score={response_json["speech_score"]["ielts_score"]["coherence"]}></ScoreBar></td>
-                                </tr>
-                                <tr>
-                                    <td className="sub_score_text">Grammar:</td>
-                                    <td className="sub_score_value">{response_json["speech_score"]["ielts_score"]["grammar"]}</td>
-                                    <td><ScoreBar score={response_json["speech_score"]["ielts_score"]["grammar"]}></ScoreBar></td>
-                                </tr>
-                                <tr>
-                                    <td className="sub_score_text">Vocabulary:</td>
-                                    <td className="sub_score_value">{response_json["speech_score"]["ielts_score"]["vocab"]}</td>
-                                    <td><ScoreBar score={response_json["speech_score"]["ielts_score"]["vocab"]}></ScoreBar></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        {/* <div>Pronunciation: {response_json["speech_score"]["ielts_score"]["pronunciation"]}</div>
-                        <div>Fluency: {response_json["speech_score"]["ielts_score"]["fluency"]}</div>
-                        <div>Coherence: {response_json["speech_score"]["ielts_score"]["coherence"]}</div>
-                        <div>Grammar: {response_json["speech_score"]["ielts_score"]["grammar"]}</div>
-                        <div>Vocabulary: {response_json["speech_score"]["ielts_score"]["vocab"]}</div> */}
+                <div className="ielts_scores">
+                    <div className="score_item">
+                        <div className="score_label">
+                            <span className="overall_score_text">IELTS overall:</span>
+                            <span className="overall_score_value">{response_json["speech_score"]["ielts_score"]["overall"]}</span>
+                        </div>
+                        <div className="score_bar">
+                            <ScoreBar score={response_json["speech_score"]["ielts_score"]["overall"]} overall={true}></ScoreBar>
+                        </div>
+                    </div>
+                    
+                    <div className="score_item">
+                        <div className="score_label">
+                            <span className="sub_score_text">Pronunciation:</span>
+                            <span className="sub_score_value">{response_json["speech_score"]["ielts_score"]["pronunciation"]}</span>
+                        </div>
+                        <div className="score_bar">
+                            <ScoreBar score={response_json["speech_score"]["ielts_score"]["pronunciation"]} overall={false}></ScoreBar>
+                        </div>
+                    </div>
+                    
+                    <div className="score_item">
+                        <div className="score_label">
+                            <span className="sub_score_text">Fluency:</span>
+                            <span className="sub_score_value">{response_json["speech_score"]["ielts_score"]["fluency"]}</span>
+                        </div>
+                        <div className="score_bar">
+                            <ScoreBar score={response_json["speech_score"]["ielts_score"]["fluency"]} overall={false}></ScoreBar>
+                        </div>
+                    </div>
+                    
+                    <div className="score_item">
+                        <div className="score_label">
+                            <span className="sub_score_text">Coherence:</span>
+                            <span className="sub_score_value">{response_json["speech_score"]["ielts_score"]["coherence"]}</span>
+                        </div>
+                        <div className="score_bar">
+                            <ScoreBar score={response_json["speech_score"]["ielts_score"]["coherence"]} overall={false}></ScoreBar>
+                        </div>
+                    </div>
+                    
+                    <div className="score_item">
+                        <div className="score_label">
+                            <span className="sub_score_text">Grammar:</span>
+                            <span className="sub_score_value">{response_json["speech_score"]["ielts_score"]["grammar"]}</span>
+                        </div>
+                        <div className="score_bar">
+                            <ScoreBar score={response_json["speech_score"]["ielts_score"]["grammar"]} overall={false}></ScoreBar>
+                        </div>
+                    </div>
+                    
+                    <div className="score_item">
+                        <div className="score_label">
+                            <span className="sub_score_text">Vocabulary:</span>
+                            <span className="sub_score_value">{response_json["speech_score"]["ielts_score"]["vocab"]}</span>
+                        </div>
+                        <div className="score_bar">
+                            <ScoreBar score={response_json["speech_score"]["ielts_score"]["vocab"]} overall={false}></ScoreBar>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="text_area">
+                <p className="title">Your answer:</p>
+                {response_json["speech_score"]["word_score_list"].map((item, index) => {
+                    var syllable_marks = new Array(item.phone_score_list.length).fill(0);
+                    var syllables = new Array(item.phone_score_list.length).fill('');
+                    var last_pos = 0;
+                    for(var syllable of item.syllable_score_list) {
+                        syllable_marks[last_pos] = syllable.phone_count;
+                        syllables[last_pos] = syllable.letters;
+                        last_pos += syllable.phone_count;
+                    }
+                                
+
+
+                    return (
+                        <span className="container" ref={(expandedItemIndex === index)? spanRef : null}>
+                            {expandedItemIndex === index && (
+                                <span className="error_detail">
+                                    <table className="phone_tab">
+                                        <thead>
+                                            <tr>
+                                                <th>Syllable</th>
+                                                <th>Phone</th>
+                                                <th>Result</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {item.phone_score_list.map((phone, idx) => (
+                                                <tr>
+                                                    {(syllable_marks[idx] > 0) && (<td rowSpan={syllable_marks[idx]}>{syllables[idx]}</td>)}
+                                                    <td className={(phone.sound_most_like === phone.phone)? "correct" : "incorrect"}>{phone.phone}</td>
+                                                    <td className={(phone.sound_most_like === phone.phone)? "correct" : "incorrect"}>{(phone.sound_most_like)? ((phone.sound_most_like === phone.phone)? 'Good' : phone.sound_most_like) : '[missing]'}</td>
+                                                </tr>
+                                            )
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </span>
+                            )}
+                            <span id={index.toString()} 
+                                className={(item.quality_score > boundary)?
+                                    ((expandedItemIndex === index)? "active_correct" : "correct") : 
+                                    ((expandedItemIndex === index)? "active_incorrect" : "incorrect")} 
+                                onClick={() => handleSpanClick(index)}>
+                                {item.word}
+                            </span>
+                            <span className="correct">{item.ending_punctuation} </span>
+                        </span>);
+                    }
+                )}
+                
             </div>
         </div>
     );
