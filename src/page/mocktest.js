efaaca4e8354de0e913b102fb5de71f838c3d45e
CeraChen <1059315177@qq.com>
2023-12-30 17:03:10 +0800

    add the feature of hiding/viewing the paper in the mock test

--- src/page/mocktest.js
+++ src/page/mocktest.js
@@ -14,10 +14,12 @@ const ANSWERING = 2;
 
 const intervalTime = 1000;
 const prepareTime = 1000 * 60 * 10;
+const mSVG = <svg t="1703230099825" className="indicate_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4207" width="30" height="40"><path d="M474.496 512l338.752-338.752-90.496-90.496L293.504 512l429.248 429.248 90.496-90.496z" p-id="4208"></path></svg>;
 
 let mTimer = null;
 let mCount, startTime, leftTime;
-let isDragging = false;
+// let isDragging = false;
+let paperShown = true;
 
 
 export default class MockTest extends React.Component {
@@ -75,38 +77,45 @@ export default class MockTest extends React.Component {
     componentDidUpdate(prevProps, prevState) {
         if(this.state.stage === PLAYING && this.state.stage !== prevState.stage) {
             console.log("playing");
-            const separator = document.getElementById("separator");
+            // const separator = document.getElementById("separator");
             const paperContainer = document.getElementById("paper_container");
             const videoContainer = document.getElementById("video_container");
+            const switchBtn = document.getElementById("switch_btn");
 
 
-            separator.addEventListener('mousedown', (event) => {
-                isDragging = true;
-                console.log("mouse down");
-                document.body.style.userSelect = 'none';
+            switchBtn.addEventListener('click', (event) => {
+                paperShown = !paperShown;
+                paperContainer.style.display = (paperShown)? 'inline' : 'none';
+                switchBtn.style.transform = (paperShown)? "none" : "scaleX(-1)";
+                // switchBtn.innerText = (paperShown)? "hide" : "view";
+                // isDragging = true;
+                // console.log("mouse down");
+                // document.body.style.userSelect = 'none';
             });
 
-            document.addEventListener('mousemove', (event) => {
-                if (!isDragging) return;
+            // document.addEventListener('mousemove', (event) => {
+            //     if (!isDragging) return;
                 
-                const x = event.clientX;
-                const containerWidth = document.documentElement.clientWidth;
-                const separatorWidth = separator.offsetWidth;
+            //     const x = event.clientX;
+            //     const containerWidth = document.documentElement.clientWidth;
+            //     const separatorWidth = separator.offsetWidth;
                 
-                const paperContainerWidth = (x / containerWidth) * 100;
-                // const videoContainerWidth = 100 - paperContainerWidth;
+            //     const paperContainerWidth = (x / containerWidth) * 100;
+            //     const videoContainerWidth = 100 - paperContainerWidth;
                 
-                paperContainer.style.width = `${paperContainerWidth}%`;
-                // videoContainer.style.width = `${videoContainerWidth}%`;
+            //     paperContainer.style.width = `${paperContainerWidth}%`;
+            //     videoContainer.style.width = `${videoContainerWidth}%`;
 
-                console.log("mouse move");
-            });
+            //     console.log("mouse move");
+            // });
 
-            document.addEventListener('mouseup', () => {
-                isDragging = false;
-                console.log("mouse up");
-                document.body.style.userSelect = 'auto';
-            });
+            // document.addEventListener('mouseup', () => {
+            //     isDragging = false;
+            //     console.log("mouse up");
+            //     document.body.style.userSelect = 'auto';
+
+            //     paperContainer.style.display = 'inline';
+            // });
         }
     }
     
@@ -213,6 +222,11 @@ export default class MockTest extends React.Component {
                 mHeading = (
                     <div className="heading">
                         <p className="part">{mPart}</p>
+                        {(this.state.part === PART_A) && 
+                            <div id="switch_btn">
+                                {mSVG}
+                                {/* {(paperShown)? "hide" : "view"} */}
+                            </div>}
                     </div>
                 );
 
@@ -222,18 +236,18 @@ export default class MockTest extends React.Component {
                             <div id="paper_container">
                                 {mQuestionArea}
                             </div>
-                            <div id="separator">
+                            {/* <div id="separator">
                                 <div className="indicator">
-                                    {/* <svg t="1703231389075" class="indicate_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8594" width="800" height="800"><path d="M664 832c10.752 0 20.096-3.968 28.096-11.904C700.096 812.096 704 802.752 704 792l0-560c0-10.816-3.904-20.16-11.904-28.096C684.096 195.904 674.752 192 664 192s-20.096 3.904-28.096 11.904L355.904 483.904C348.032 491.84 344 501.184 344 512c0 10.752 3.968 20.096 11.904 28.096l280 280C643.904 828.032 653.248 832 664 832z" p-id="8595"></path></svg> */}
                                     <svg t="1703230099825" className="indicate_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4207" width="200" height="200"><path d="M474.496 512l338.752-338.752-90.496-90.496L293.504 512l429.248 429.248 90.496-90.496z" p-id="4208"></path></svg>
-                                    {/* <svg t="1703230158772" class="indicate_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4585" width="200" height="200"><path d="M512 2.373117C230.785632 2.373117 2.966396 230.192352 2.966396 511.406721S230.785632 1020.440324 512 1020.440324s509.033604-227.819235 509.033604-509.033603S793.214368 2.373117 512 2.373117z m186.289687 719.054461c15.425261 15.425261 15.425261 40.34299 0 55.768251L622.349942 853.135574a39.393743 39.393743 0 0 1-55.76825 0L331.643105 619.383546c-1.186559-1.186559-2.373117-1.186559-2.373117-2.373117l-77.126303-77.126304c-3.559676-3.559676-7.119351-8.30591-8.30591-13.052144-5.932793-14.238702-3.559676-32.03708 8.30591-42.716106l77.126303-77.126304c1.186559-1.186559 3.559676-3.559676 5.932793-4.746234l232.565469-232.565469c15.425261-15.425261 40.34299-15.425261 55.768251 0l77.126303 77.126303c15.425261 15.425261 15.425261 40.34299 0 55.768251L489.455388 511.406721l208.834299 210.020857z" p-id="4586"></path></svg> */}
-                                    {/* <svg t="1703230581648" class="indicate_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4877" width="200" height="200"><path d="M210.4 511.1 641.3 80.2c22.8-22.8 59.8-22.8 82.6 0 22.8 22.8 22.8 59.8 0 82.6L375.7 511.1l348.3 348.3c22.8 22.8 22.8 59.8 0 82.6-22.8 22.8-59.8 22.8-82.6 0L210.4 511.1 210.4 511.1zM210.4 511.1" p-id="4878"></path></svg> */}
                                 </div>
-                            </div>
+                            </div> */}
                             <div id="video_container">
                                 <p className="guide">Starter video:</p>
-                                <video className="starter_video"></video>
-                                {/* <video src="https://video-ssl.itunes.apple.com/itunes-assets/Video124/v4/15/e2/80/15e280e2-7d82-6b9b-2cb1-90df023eaa29/mzvf_10142644751851384725.720w.h264lc.U.p.m4v" autoPlay></video> */}
+                                {/* <video className="starter_video"></video> */}
+                                <div className="video_subcontainer">
+                                    <video id="video_player" src="https://rr4---sn-i3b7knld.googlevideo.com/videoplayback?expire=1703942923&ei=q8aPZbzzOoGa1d8PzsawMA&ip=43.129.29.153&id=o-ALxqmNCnTAT36RnxDFI-D7pUJIUTOGm51vjCOA46-b7i&itag=136&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=I0&mm=31%2C29&mn=sn-i3b7knld%2Csn-i3belnlz&ms=au%2Crdu&mv=m&mvi=4&pl=21&initcwndbps=623750&vprv=1&svpuc=1&mime=video%2Fmp4&gir=yes&clen=12345656&dur=237.070&lmt=1703028113368785&mt=1703920860&fvip=2&keepalive=yes&fexp=24007246&c=IOS&txp=4535434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAMrBZ6GVs4QRHCEp0-OK69jfliPlt_4cYZ2ZHC-j_9UIAiEA7zM9lUYBx_YmZGQ78RMitbacPdP4wvSxuum_qBZ55Vg%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AAO5W4owRQIhAPEH6osM_v-0UoE9KXvSbPX134lBOX5zoNQZxrw6YHvbAiAgS38KCuAeLXcNBRMpJ8XjLz89ZiZIFNQFdZDvLiOf6g%3D%3D" autoPlay></video>
+                                </div>
+                                {/* https://video-ssl.itunes.apple.com/itunes-assets/Video124/v4/15/e2/80/15e280e2-7d82-6b9b-2cb1-90df023eaa29/mzvf_10142644751851384725.720w.h264lc.U.p.m4v" autoPlay></video> */}
                             </div>
                         </div>
                     </div>
